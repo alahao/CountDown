b39bb7cedd13181027de96717d6521e629c426b2
NAN ZI WANG <zwang@NANZIs-MacBook-Pro.local>
2017-04-29 08:58:42 -0400

    no message

---
+++ scheduleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  scheduleTableViewCell.swift
+//  CountDown
+//
+//  Created by NANZI WANG on 4/27/17.
+//  Copyright © 2017 PrettyMotion. All rights reserved.
+//
+
+import UIKit
+
+class scheduleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
